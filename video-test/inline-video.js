e1423ad9319bb4b82e1d287e0fe6987d1c9ea8a0
Bartek Kozera <bartek.kozera@gmail.com>
2016-03-08 14:04:43 +0100

    ...

--- video-test/inline-video.js
+++ video-test/inline-video.js
@@ -113,7 +113,7 @@ var InlineVideo = (function () {
             if (elapsed > 1.0 / this.framerate) {
                 this.last_frame_time = time;
                 this.video.currentTime += elapsed;
-                this.canvas.getContext('2d').drawImage(this.video, 0, 0, window.innerWidth, 200);                
+                this.canvas.getContext('2d').drawImage(this.video, 0, 0, 414, 200);                
             }
             // if we are at the end of the video stop
             if (this.video.currentTime < this.video.duration) {
