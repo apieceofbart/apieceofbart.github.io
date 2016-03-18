2bd1a3dec4c9230405e65a591c1baf2d8902fad1
Bartek Kozera <bartek.kozera@gmail.com>
2016-03-18 11:01:18 +0100

    fixing poster

--- video-test/inline-video.js
+++ video-test/inline-video.js
@@ -37,11 +37,9 @@ var InlineVideo = (function () {
             this.audio = document.querySelector(audio_identifier);
         }
         this.ios = fake_ios || /iPad|iPhone|iPod/.test(navigator.platform);        
-        /*var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;*/
         // On IOS it will be webkitRequestAnimationFrame. Hopefully they will drop the prefix in the future
         // !Notice: Dropped other prefix since this is for IOS only
-        /*alert('navigator:', navigator.platform);
-        alert("is this ios:", this.ios);*/
+
         if (!window.requestAnimationFrame) {
             window.requestAnimationFrame = window.webkitRequestAnimationFrame;
             window.cancelRequestAnimationFrame = window.webkitCancelRequestAnimationFrame;
